0f9468473e0a53541cf534a1861346c56fd1bdfc
allas <allassantos1000@gmail.com>
2020-11-20 20:49:20 -0300

    Resolução do exercício Hello World!

---
+++ src/iniciante/uri1000/Main.java
@@ -0,0 +1,10 @@
+package iniciante.uri1000;
+
+public class Main {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.printf("Hello World!%n"); // exibir uma mensagem na janela de comando
+    }
+}
