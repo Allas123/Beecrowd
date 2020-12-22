ac4dbd27a03db294023fc38336e4d72a557a3d61
Allas Santos <allassantos1000@gmail.com>
2020-12-22 20:25:50 -0300

    Resolução do exercício Distância

--- src/iniciante/uri1016/Main.java
+++ src/iniciante/uri1016/Main.java
@@ -7,5 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        int minuto;
+        int distancia;
+
+        distancia = input.nextInt();
+
+        minuto = distancia *  60 / 30 ;
+
+        System.out.printf("%d minutos%n", minuto);
+
+        input.close();
     }
 }
