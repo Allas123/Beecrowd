6551de63cb07822180c3e38a74829b1b9b36e8f1
Allas Santos <allassantos1000@gmail.com>
2020-12-30 00:55:23 -0300

    Resolução do exercício Média 1

--- src/iniciante/uri1005/Main.java
+++ src/iniciante/uri1005/Main.java
@@ -1,31 +1,27 @@
 package iniciante.uri1005;
 
-import java.util.Scanner; // programa utiliza a classe Scanner
-
+import java.util.Scanner;
 
 public class Main {
 
-    // método main inicia a execução do aplicativo Java
     public static void main(String[] args) {
 
-        // cria um Scanner para obter entrada a partir da janela de comando
         Scanner input = new Scanner(System.in);
 
-        double a; // primeira nota a fazer a média
-        double b; // segundo nota a fazer a média
-        double media; // cálculo da média
+        double a;
+        double b;
+        double media;
 
-        a = input.nextDouble(); // lê primeira nota fornecindo pelo usuário e armazenando na variável a
-        b = input.nextDouble(); // lê segunda nota fornecindo pelo usuário e armazenando na variável b
+        a = input.nextDouble();
+        b = input.nextDouble();
 
-        a *= 3.5; // atribuído peso da primeira nota
-        b *= 7.5; // atribuído peso da segunda nota
+        a *= 3.5;
+        b *= 7.5;
 
-        media = ((a + b) / 11); // fazer cálculo da média, depois  armazenar na variável media
+        media = ((a + b) / 11);
 
-        System.out.printf("MEDIA = %.5f%n", media); // exiber o valor da variável media
+        System.out.printf("MEDIA = %.5f%n", media);
 
-        // fechar input
         input.close();
     }
 }
