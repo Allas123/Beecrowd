ccf0ee7f88047d649a82e4ff37926d93c17aa399
Allas Santos <allassantos1000@gmail.com>
2020-12-30 00:54:47 -0300

    Resolução do exercício Produto Simples

--- src/iniciante/uri1004/Main.java
+++ src/iniciante/uri1004/Main.java
@@ -1,29 +1,24 @@
 package iniciante.uri1004;
 
-import java.util.Scanner; // programa utiliza a classe Scanner
+import java.util.Scanner;
 
 public class Main {
 
-    // método main inicia a execução do aplicativo Java
     public static void main(String[] args) {
 
-        // cria um Scanner para obter entrada a partir da janela de comando
         Scanner input = new Scanner(System.in);
 
-        int primeiroNumero; // primeiro número a ser multiplicado
-        int segundoNumero; // segundo número a ser multiplicado
-        int prod; // cálculo da multiplicação
+        int primeiroNumero;
+        int segundoNumero;
+        int prod;
 
-        primeiroNumero = input.nextInt(); // lê primeiro número fornecindo pelo usuário e armazenando na variável primeiroNumero
-        segundoNumero = input.nextInt(); // lê segundo número fornecindo pelo usuário e armazenando na variável segundoNumero
+        primeiroNumero = input.nextInt();
+        segundoNumero = input.nextInt();
 
-        // fazendo a multiplicação da variáveis, primeroNumero e segundoNumero, depois armazenar na variavel prod
         prod = primeiroNumero * segundoNumero;
 
-        System.out.printf("PROD = %d%n", prod); // exiber o valor da variável prod
+        System.out.printf("PROD = %d%n", prod);
 
-        // fechar input
         input.close();
-
     }
 }
