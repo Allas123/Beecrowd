0929ba6f4c9bc79025b8f6df0ae3276668f77147
Allas Santos <allassantos1000@gmail.com>
2021-03-18 17:43:43 -0300

    Resolução do exercício Múltiplos

---
+++ src/iniciante/uri1044/Main.java
@@ -0,0 +1,29 @@
+package iniciante.uri1044;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int a;
+    int b;
+    int resto;
+
+    a = input.nextInt();
+    b = input.nextInt();
+
+    resto = (Math.max(a, b) % Math.min(a , b));
+
+    if (resto == 0 ) {
+        System.out.printf("Sao Multiplos%n");
+
+    } else {
+        System.out.printf("Nao sao Multiplos%n");
+    }
+
+    input.close();
+    }
+}
