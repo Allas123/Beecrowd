81e768709dc0bc6d48888d1d3728026ca2222e91
Allas Santos <40401422+Allas123@users.noreply.github.com>
2021-04-05 21:11:46 -0300

    Resolução do exercício Positivos e Média

---
+++ src/iniciante/uri1064/Main.java
@@ -0,0 +1,33 @@
+package iniciante.uri1064;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+
+        int i = 1;
+        float numero;
+        int positivo = 0;
+        float media;
+        float soma = 0;
+
+        while(i <= 6 ) {
+            numero = input.nextFloat();
+
+            if(numero > 0) {
+                positivo += 1;
+                soma += numero;
+
+            }
+            i++;
+        }
+
+        media = soma / positivo;
+
+        System.out.printf("%d valores positivos%n", positivo);
+        System.out.printf("%.1f%n",media);
+    }
+}
