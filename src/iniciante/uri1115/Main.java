573f380ec18732c296a05859ef95b48c3d7ed009
Allas Santos <40401422+Allas123@users.noreply.github.com>
2021-09-25 19:36:39 -0300

    Resolução do exercício quadrante

---
+++ src/iniciante/uri1115/Main.java
@@ -0,0 +1,41 @@
+package iniciante.uri1115;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        while (true) {
+            x = input.nextInt();
+            y = input.nextInt();
+
+            if (x == 0 || y == 0) {
+                break;
+            }
+
+            if (x > 0 && y > 0) {
+                System.out.printf("primeiro%n");
+            }
+
+            if (x < 0 && y > 0) {
+                System.out.printf("segundo%n");
+            }
+
+            if (x < 0 && y < 0) {
+                System.out.printf("terceiro%n");
+            }
+
+            if (x > 0 && y < 0) {
+                System.out.printf("quarto%n");
+            }
+        }
+
+        input.close();
+    }
+}
